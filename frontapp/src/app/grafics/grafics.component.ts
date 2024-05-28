ab15cee7944c018bdf8caaefa6c9d3d9f9fdf950
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-28 17:29:39 -0500

    Update styles

--- frontapp/src/app/grafics/grafics.component.ts
+++ frontapp/src/app/grafics/grafics.component.ts
@@ -7,9 +7,7 @@ import { Chart } from 'chart.js';
   templateUrl: './grafics.component.html',
   styleUrls: ['./grafics.component.css']
 })
-export class GraficsComponent implements AfterViewInit {
-  @ViewChild('canvas') canvasRef: ElementRef;
-
+export class GraficsComponent  {
   diasSemana: string[] = [];
   totalProductos: number[] = [];
   chart: Chart;
@@ -18,46 +16,6 @@ export class GraficsComponent implements AfterViewInit {
 
   constructor(private http: HttpClient) { }
 
-  ngAfterViewInit(): void {
-    this.http.get<any[]>('http://localhost:8081/api/producto/product/dias/semana')
-      .subscribe(data => {
-        data.forEach(item => {
-          this.diasSemana.push(item[0]);
-          this.totalProductos.push(item[1]);
-        });
-        this.createChart();
-      });
-  }
-
-  createChart(): void {
-    if (this.canvasRef) {
-      const canvas = this.canvasRef.nativeElement;
-      const ctx = canvas.getContext('2d');
-      
-      this.chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: this.diasSemana,
-          datasets: [{
-            label: 'Total de productos',
-            data: this.totalProductos,
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
-    } else {
-      console.error('canvasRef is undefined');
-    }
-  }
   ngOnInit(): void {
     this.http.get<any[]>('http://localhost:8081/api/producto/product/dias/semana')
       .subscribe(data => {
@@ -72,7 +30,8 @@ export class GraficsComponent implements AfterViewInit {
           datasets: [
             {
               label: 'Total de productos',
-              backgroundColor: '#42A5F5',
+              backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+              borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
               data: valores
             }
           ]
