cbf79e0bfc4be6d00fd52e1465b2a905f3fa9008
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-06-04 11:15:26 -0500

    Update UsuariosModel.java

--- backapp/src/main/java/com/appback/backapp/model/UsuariosModel.java
+++ backapp/src/main/java/com/appback/backapp/model/UsuariosModel.java
@@ -1,4 +1,18 @@
 package com.appback.backapp.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_usuarios")
+@Data
 public class UsuariosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idusuario;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    
 }
